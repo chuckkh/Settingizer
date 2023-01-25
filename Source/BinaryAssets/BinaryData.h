5214c0643bab76ad2f602a9183369af9f17004bd
ChuckkLaptop <chuckk.hubbard.laptop@gmail.com>
2023-01-25 10:10:20 +0200

    New knobs

--- Source/BinaryAssets/BinaryData.h
+++ Source/BinaryAssets/BinaryData.h
@@ -4,7 +4,16 @@
 
 namespace BinaryData
 {
-    extern const char*  knob001_png;
-    const int           knob001_pngSize = 1333;
+    extern const char*  knobHb001_png;
+    const int           knobHb001_pngSize = 8661;
+
+    extern const char*  knobHw001_png;
+    const int           knobHw001_pngSize = 8674;
+
+    extern const char*  knobVb001_png;
+    const int           knobVb001_pngSize = 8621;
+
+    extern const char*  knobVw001_png;
+    const int           knobVw001_pngSize = 8742;
 
 }
